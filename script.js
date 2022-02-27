0d463b053ce602c7a12f4bf827f5789dc6d725a3
Xd666 / Naveen <63446662+Naveen4703@users.noreply.github.com>
2022-02-27 08:23:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ var today = new Date();
    var hour = today.getHours();
    var minute = today.getMinutes();
 
-var url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text= Someone from '+city1+" with IP - "+data1+' has checked flames on your website with names '+firstName+' '+secondName+' at '+ day + "/" + month + "/" + year + "  " + hour + ":" + minute + " and their result was " +final;
+var url = 'https://api.telegram.org/bot'+token+'/sendMessage?chat_id='+chat_id+'&text= Someone with IP - '+ip1+' has checked flames on your website with names '+firstName+' '+secondName+' at '+ day + "/" + month + "/" + year + "  " + hour + ":" + minute + " and their result was " +final;
 
 let api = new XMLHttpRequest();
 
